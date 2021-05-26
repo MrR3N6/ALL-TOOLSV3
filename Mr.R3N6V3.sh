b6b08d5ea64947cd9ec952fa43ffdde8f11b58c1
MrR3N6 <84824838+MrR3N6@users.noreply.github.com>
2021-05-26 09:59:06 +0700

    Add files via upload

---
+++ Mr.R3N6V3.sh
@@ -0,0 +1,67 @@
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+sleep 1
+echo
+toilet -f big  -F gay  Mr.R3N6 System
+echo
+toilet -f big -F gay stabilizer sinyal
+neofetch
+date
+echo $blue"selamat datang di my script"
+echo  $blue'==========+Mr.R3N6+========================='
+echo        [+]$green'AUTHOR :Mr.R3N6 [✓]'
+echo        [+]$BLUE'NO WA :083845601628 [✓]'
+echo        [+]$yellow'INSTAGRAM : ranggaofficial27 [✓]'
+echo $green'=========÷Mr.R3N6+========================='
+sleep 2
+echo $green"silahkan pilih toolsnya"
+echo
+echo $cyan"1.) setabilkan jaringan"
+echo
+echo $gray"2.) install all"
+echo
+echo $yellow"0.) keluar aja ya"
+echo
+echo $white
+read -p "PilihMana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Mr.R3N6
+echo $green"setabilkan jaringan by Mr.R3N6"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay  ALL-UPDATE Mr.R3N6
+echo $blue"ALL UPDATE UNTUK TERMUX"
+sleep2
+pkg install python
+pkg install python2
+pkg install php
+pkg install bash
+pkg install ssh
+pkg install figlet
+gem install lolcat
+pip2 install requests
+fi
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "MR.R3N6"
+echo $white "to be continued"
+sleep 3
+exit
+fi
